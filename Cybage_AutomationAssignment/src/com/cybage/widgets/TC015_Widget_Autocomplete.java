d9906c9b65c20a7967cd897da255a4a6cf24c4d4
aadityn <aadityn@Aadityn-VD3.cybage.com>
2022-10-28 11:37:09 +0530

    Autocomplete code Committed

--- Cybage_AutomationAssignment/src/com/cybage/widgets/TC015_Widget_Autocomplete.java
+++ Cybage_AutomationAssignment/src/com/cybage/widgets/TC015_Widget_Autocomplete.java
@@ -1,15 +1,15 @@
 package com.cybage.widgets;
 
-import java.awt.List;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.*;
 
 
 public class TC015_Widget_Autocomplete 
@@ -18,35 +18,65 @@ public class TC015_Widget_Autocomplete
 	public static WebDriver driver;
 	public static void main(String[] args) throws InterruptedException 
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\aadityn\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\aadityn\\\\Downloads\\\\chromedriver_win32\\\\chromedriver.exe");
 		driver= new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://demoqa.com/auto-complete");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
-		TC015_Widget_Autocomplete complete= new TC015_Widget_Autocomplete();
-		
-		//Entering the Names in the Fields 
-		complete.ColorFill("Black");
+		TC015_Widget_Autocomplete twa= new TC015_Widget_Autocomplete();
+		twa.autocomplete();
 		
 	//	driver.quit();
-
+		
+		
 	}
 	
-	public void ColorFill(String textToSelect)
+	public void autocomplete()
 	{
-		WebElement multiplecolor=driver.findElement(By.xpath("//span[contains(text(),'Type multiple color names')]/following-sibling::div/div/div[1]"));
+		WebElement ele1=driver.findElement(By.xpath("//div[@id='autoCompleteMultipleContainer']"));
 		
 		WebDriverWait wait= new WebDriverWait(driver, 20);
-		wait.until(ExpectedConditions.visibilityOf(multiplecolor));
+		wait.until(ExpectedConditions.visibilityOf(ele1));
 		
-		List<WebElement> optionsToSelect = multiplecolor.findElements(By.tagName("div"));
-		for(WebElement option : optionsToSelect){
-	        if(option.getText().equals(textToSelect)) {
-	        	System.out.println("Trying to select: "+textToSelect);
-	            option.click();
-	            break;
-	
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click()", ele1);
+		ele1.sendKeys("a");
+		
+		SelectOption("Black");
+		
+		
 	}
 	
+	public void SelectOption(String color)
+	{
+		try 
+		{
+			WebElement Multichoice = driver.findElement(By.cssSelector(".auto-complete__value-container.auto-complete__value-container--is-multi.auto-complete__value-container--has-value.css-1hwfws3"));
+			
+			WebDriverWait wait= new WebDriverWait(driver, 20);
+			wait.until(ExpectedConditions.visibilityOf(Multichoice));
+
+			List<WebElement> optionsToSelect = Multichoice.findElements
+			(By.xpath("//div[@class='auto-complete__value-container auto-complete__value-container--is-multi auto-complete__value-container--has-value css-1hwfws3']/div"));
+			for(WebElement option : optionsToSelect)
+			{
+		        if(option.getText().equals(color)) 
+		        {
+		        	System.out.println("Trying to select: "+color);
+		            option.click();
+		            break;
+		        }
+		    }
+		}
+		catch (NoSuchElementException e) 
+		{
+			System.out.println(e.getStackTrace());
+		}
+		catch (Exception e) 
+		{
+			System.out.println(e.getStackTrace());
+		}
+	}
 }
+
